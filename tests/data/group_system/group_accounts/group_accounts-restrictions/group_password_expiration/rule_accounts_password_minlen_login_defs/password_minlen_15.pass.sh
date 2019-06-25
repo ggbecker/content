a2f77b6ddc9c3a2595d03fae5dccfa32df53085b
Gabriel Becker <ggasparb@redhat.com>
2019-06-25 15:17:12 +0200

    Port minlen tests to RHEL8.

--- tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_15.pass.sh
+++ tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_15.pass.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# profiles = xccdf_org.ssgproject.content_profile_standard
-# platform = multi_platform_fedora
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+# platform = multi_platform_fedora,Red Hat Enterprise Linux 8
 
 if grep -q "^PASS_MIN_LEN" /etc/login.defs; then
-	sed -i "s/^PASS_MIN_LEN.*/PASS_MIN_LEN 12/" /etc/login.defs
+	sed -i "s/^PASS_MIN_LEN.*/PASS_MIN_LEN 15/" /etc/login.defs
 else
-	echo "PASS_MIN_LEN 12" >> /etc/login.defs
+	echo "PASS_MIN_LEN 15" >> /etc/login.defs
 fi
