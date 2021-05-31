9b1213ca586643710a6680f8a22008a86ccd177e
Watson Sato <wsato@redhat.com>
2021-05-31 17:27:41 +0200

    Move product dependency closer to declaration
    A dependency on rule playbooks target was being added from a
    conditional branch related to profile playbooks.
    It caused issues when building profile playbooks but not rule playbooks,
    the rule playbooks target would not exist, but still be added as
    dependency.

--- cmake/SSGCommon.cmake
+++ cmake/SSGCommon.cmake
@@ -277,6 +277,10 @@ macro(ssg_build_ansible_playbooks PRODUCT)
         DEPENDS "${SSG_BUILD_SCRIPTS}/build_rule_playbooks.py"
         COMMENT "[${PRODUCT}-content] Generating Ansible Playbooks"
     )
+    add_custom_target(
+        generate-${PRODUCT}-ansible-playbooks
+        DEPENDS "${ANSIBLE_PLAYBOOKS_DIR}"
+    )
     add_test(
         NAME "${PRODUCT}-ansible-playbooks-generated-for-all-rules"
         COMMAND env "PYTHONPATH=$ENV{PYTHONPATH}" "${PYTHON_EXECUTABLE}" "${CMAKE_SOURCE_DIR}/tests/ansible_playbooks_generated_for_all_rules.py" --build-dir "${CMAKE_BINARY_DIR}" --product "${PRODUCT}"
@@ -766,12 +770,12 @@ macro(ssg_build_product PRODUCT)
     ssg_build_ocil_unlinked(${PRODUCT})
     ssg_build_remediations(${PRODUCT})
 
-    add_custom_target(
-        generate-${PRODUCT}-ansible-playbooks
-        DEPENDS "${ANSIBLE_PLAYBOOKS_DIR}"
-    )
     if ("${PRODUCT_ANSIBLE_REMEDIATION_ENABLED}" AND SSG_ANSIBLE_PLAYBOOKS_PER_RULE_ENABLED)
         ssg_build_ansible_playbooks(${PRODUCT})
+        add_dependencies(
+            ${PRODUCT}-content
+            generate-${PRODUCT}-ansible-playbooks
+        )
     endif()
     ssg_build_xccdf_with_remediations(${PRODUCT})
     ssg_build_oval_unlinked(${PRODUCT})
@@ -802,10 +806,6 @@ macro(ssg_build_product PRODUCT)
     add_dependencies(zipfile "generate-ssg-${PRODUCT}-ds.xml")
 
     if ("${PRODUCT_ANSIBLE_REMEDIATION_ENABLED}" AND SSG_ANSIBLE_PLAYBOOKS_ENABLED)
-        add_dependencies(
-            ${PRODUCT}-content
-            generate-${PRODUCT}-ansible-playbooks
-        )
         ssg_build_profile_playbooks(${PRODUCT})
         add_custom_target(
             ${PRODUCT}-profile-playbooks
